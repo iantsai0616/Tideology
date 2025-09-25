927d67a9d5ecefc053794937276916cabcb88de5
iantsai0616 <iantsai0616@gmail.com>
2025-09-25 23:08:07 +0800

    fixed template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define int long long
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define F first
+#define S second
+#define pii pair<int,int>
+#define chmax(x,y) x=x>y?x:y;
+#define chmin(x,y) x=x<y?x:y;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+#define vi vector<int>
+void solve(){
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+}
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	int t = 1;
+	while(t--) solve();
 }
