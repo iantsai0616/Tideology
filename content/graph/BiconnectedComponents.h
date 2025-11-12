963093b1c310be657bf65c25d77975104eb6fa15
iantsai0616 <iantsai0616@gmail.com>
2025-11-12 23:48:20 +0800

    fixed bcc

--- content/graph/BiconnectedComponents.h
+++ content/graph/BiconnectedComponents.h
@@ -18,36 +18,43 @@
  * Status: tested during MIPT ICPC Workshop 2017
  */
 #pragma once
-
-vi num, st;
-vector<vector<pii>> ed;
-int Time;
-template<class F>
-int dfs(int at, int par, F& f) {
-	int me = num[at] = ++Time, top = me;
-	for (auto [y, e] : ed[at]) if (e != par) {
-		if (num[y]) {
-			top = min(top, num[y]);
-			if (num[y] < me)
-				st.push_back(e);
-		} else {
-			int si = sz(st);
-			int up = dfs(y, e, f);
-			top = min(top, up);
-			if (up == me) {
-				st.push_back(e);
-				f(vi(st.begin() + si, st.end()));
-				st.resize(si);
-			}
-			else if (up < me) st.push_back(e);
-			else { /* e is a bridge */ }
-		}
-	}
-	return top;
-}
-
-template<class F>
-void bicomps(F f) {
-	num.assign(sz(ed), 0);
-	rep(i,0,sz(ed)) if (!num[i]) dfs(i, -1, f);
-}
+struct BCC { // 0-based, remember to build
+  int n, nbcc; // note for isolated point
+  vector <vector <int>> g, _g; // id >= n: bcc
+  vector <int> pa, dep, low, stk, pa2, dep2;
+  void dfs(int v, int p) {
+    dep[v] = low[v] = ~p ? dep[p] + 1 : 0;
+    stk.pb(v), pa[v] = p;
+    for (int u : g[v]) if (u != p) {
+      if (low[u] == -1) {
+        dfs(u, v), low[v] = min(low[v], low[u]);
+        if (low[u] >= dep[v]) {
+          int id = nbcc++, x;
+          do {
+            x = stk.back(), stk.pop_back();
+            _g[id + n].pb(x), _g[x].pb(id + n);
+          } while (x != u);
+          _g[id + n].pb(v), _g[v].pb(id + n);
+        }
+      } else low[v] = min(low[v], dep[u]);
+    }
+  }
+  bool is_cut(int x) { return sz(_g[x]) != 1; }
+  vector <int> bcc(int id) { return _g[id + n]; }
+  int bcc_id(int u, int v) {
+    return pa2[dep2[u] < dep2[v] ? v : u] - n; }
+  void dfs2(int v, int p) {
+    dep2[v] = ~p ? dep2[p] + 1 : 0, pa2[v] = p;
+    for (int u : _g[v]) if (u != p) dfs2(u, v);
+  }
+  void build() {
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) if (low[i] == -1)
+      dfs(i, -1), dfs2(i, -1);
+  }
+  void add_edge(int u, int v) {
+    g[u].pb(v), g[v].pb(u); }
+  BCC (int _n) : n(_n), nbcc(0), g(n), _g(2 * n),
+    pa(n), dep(n), low(n), stk(), pa2(n * 2),
+    dep2(n * 2) {}
+};
