88e0a5249cc28ffcea5870b1a4b30f1deae5c12c
iantsai0616 <iantsai0616@gmail.com>
2025-11-14 15:11:05 +0800

    fixed lct

---
+++ content/strings/Sam.h
@@ -0,0 +1,67 @@
+// == PART HASH ==
+struct exSAM {
+  const int CNUM = 26;
+  // len: maxlength, link: fail link
+  // lenSorted: topo order, cnt: occur
+  vector<int> len, link, lenSorted, cnt;
+  vector<vector<int>> next;
+  int total = 0;
+  int newnode() {
+    return total++; 
+  }
+  void init(int n) { // total number of characters
+    len.assign(2 * n, 0); link.assign(2 * n, 0);
+    lenSorted.assign(2 * n, 0); cnt.assign(2 * n, 0);
+    next.assign(2 * n, vector<int>(CNUM));
+    newnode(), link[0] = -1; 
+  }
+// == PART HASH ==
+  int insertSAM(int last, int c) { // SCOPE HASH
+    // not exSAM: cur = newnode(), p = last
+    int cur = next[last][c];
+    len[cur] = len[last] + 1;
+    int p = link[last];
+    while (p != -1 && !next[p][c])
+      next[p][c] = cur, p = link[p];
+    if (p == -1) return link[cur] = 0, cur;
+    int q = next[p][c];
+    if (len[p] + 1 == len[q]) return link[cur] = q, cur;
+    int clone = newnode();
+    for (int i = 0; i < CNUM; ++i)
+      next[clone][i] = len[next[q][i]] ? next[q][i] : 0;
+    len[clone] = len[p] + 1;
+    while (p != -1 && next[p][c] == q)
+      next[p][c] = clone, p = link[p];
+    link[link[cur] = clone] = link[q];
+    link[q] = clone;
+    return cur;
+  }
+  void insert(const string &s) { // SCOPE HASH
+    int cur = 0;
+    for (auto ch : s) {
+      int &nxt = next[cur][int(ch - 'a')];
+      if (!nxt) nxt = newnode();
+      cnt[cur = nxt] += 1;
+    }
+  }
+// == PART HASH ==
+  void build() {
+    queue<int> q;
+    q.push(0);
+    while (!q.empty()) {
+      int cur = q.front();
+      q.pop();
+      for (int i = 0; i < CNUM; ++i)
+        if (next[cur][i])
+          q.push(insertSAM(cur, i));
+    }
+    vector<int> lc(total);
+    for (int i = 1; i < total; ++i) ++lc[len[i]];
+    partial_sum(iter(lc), lc.begin());
+    for (int i = 1; i < total; ++i) lenSorted[--lc[len[i]]] = i;
+  }
+  void solve() {
+    for (int i = total - 2; i >= 0; --i)
+      cnt[link[lenSorted[i]]] += cnt[lenSorted[i]];
+  }
+};
