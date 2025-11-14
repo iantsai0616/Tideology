c9b371e38226f68ad8acb782e3babd209fe37511
iantsai0616 <iantsai0616@gmail.com>
2025-11-14 20:56:51 +0800

    added VirtualTree

--- content/graph/SCC.h
+++ content/graph/SCC.h
@@ -1,41 +1,31 @@
-/**
- * Author: Lukas Polacek
- * Date: 2009-10-28
- * License: CC0
- * Source: Czech graph algorithms book, by Demel. (Tarjan's algorithm)
- * Description: Finds strongly connected components in a
- * directed graph. If vertices $u, v$ belong to the same component,
- * we can reach $u$ from $v$ and vice versa.
- * Usage: scc(graph, [\&](vi\& v) { ... }) visits all components
- * in reverse topological order. comp[i] holds the component
- * index of a node (a component only has edges to components with
- * lower index). ncomps will contain the number of components.
- * Time: O(E + V)
- * Status: Bruteforce-tested for N <= 5
- */
-#pragma once
-
-vi val, comp, z, cont;
-int Time, ncomps;
-template<class G, class F> int dfs(int j, G& g, F& f) {
-	int low = val[j] = ++Time, x; z.push_back(j);
-	for (auto e : g[j]) if (comp[e] < 0)
-		low = min(low, val[e] ?: dfs(e,g,f));
-
-	if (low == val[j]) {
-		do {
-			x = z.back(); z.pop_back();
-			comp[x] = ncomps;
-			cont.push_back(x);
-		} while (x != j);
-		f(cont); cont.clear();
-		ncomps++;
-	}
-	return val[j] = low;
-}
-template<class G, class F> void scc(G& g, F f) {
-	int n = sz(g);
-	val.assign(n, 0); comp.assign(n, -1);
-	Time = ncomps = 0;
-	rep(i,0,n) if (comp[i] < 0) dfs(i, g, f);
-}
+struct SCC{ // 0-based, output reversed topo order
+  int n, cnt = 0;
+  vector<vector<int>> g;
+  vector<int> sccid;
+  explicit SCC(int _n): n(_n), g(n), sccid(n, -1) {}
+  void add_edge(int u, int v){
+    g[u].pb(v);
+  }
+  void build(){
+    vector<int> in(n, -1), low(n), stk;
+    vector<bool> instk(n);
+    int ts = 0;
+    auto dfs1 = [&](auto dfs, int now) -> void{
+      stk.pb(now); instk[now] = true;
+      in[now] = low[now] = ts++;
+      for(int i : g[now]){
+        if(in[i] == -1)
+          dfs(dfs, i), low[now] = min(low[now], low[i]);
+        else if(instk[i] && in[i] < in[now])
+          low[now] = min(low[now], in[i]);
+      }
+      if(low[now] == in[now]){
+        for(; stk.back() != now; stk.pop_back())
+          sccid[stk.back()] = cnt, instk[stk.back()] = false;
+        sccid[now] = cnt++, instk[now] = false, stk.pop_back();
+      }
+    };
+    for(int i = 0; i < n; i++)
+      if(in[i] == -1) dfs1(dfs1, i);
+  }
+};
---
+++ content/graph/VirtualTree.h
@@ -0,0 +1,27 @@
+vector<int> vG[N];
+int top, st[N];
+int vrt = -1;
+void insert(int u) {
+	if (top == -1) return st[++top] = vrt = u, void();
+	int p = LCA(st[top], u);
+    if(dep[vrt] > dep[p]) vrt = p;
+	if (p == st[top]) return st[++top] = u, void();
+	while (top >= 1 && dep[st[top - 1]] >= dep[p])
+		vG[st[top - 1]].pb(st[top]), --top;
+	if (st[top] != p)
+		vG[p].pb(st[top]), --top, st[++top] = p;
+	st[++top] = u;
+}
+void reset(int u) {
+	for (int i : vG[u]) reset(i);
+	vG[u].clear();
+}
+void solve(vector<int> &v) {
+	top = -1;
+	sort(iter(v),
+			[&](int a, int b) { return dfn[a] < dfn[b]; });
+	for (int i : v) insert(i);
+	while (top > 0) vG[st[top - 1]].pb(st[top]), --top;
+	// do something
+	reset(vrt);
+}
